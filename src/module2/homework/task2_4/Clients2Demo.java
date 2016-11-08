88d0d303452d893dac2ef8ab5a50d33659e70bd2
Denys Sheptytskyi <neoxesis@mail.ru>
2016-11-08 23:22:40 +0200

    Homework task 2_4 -sum parameter in fund() method is now double -fund()method now accepts clientName parameter

--- src/module2/homework/task2_4/Clients2Demo.java
+++ src/module2/homework/task2_4/Clients2Demo.java
@@ -1,22 +1,12 @@
 package module2.homework.task2_4;
 
-/**
- * The Clients2Demo class is used to test the functionality of the Clients2 class
- *
- * @author Denys Sheptytskyi
- */
 class Clients2Demo {
-    public static void main(String[] args) {
-        /*
-         * creates 3 clients as objects of Clients2 class and tries to fund their accounts
-         */
-        Clients2 person1 = new Clients2("Ann");
-        Clients2 person2 = new Clients2("Oww");
-        Clients2 person3 = new Clients2("Denys");       // non-existent client name
 
-        person1.fund(100);
-        person2.fund(490);
-        person3.fund(10); //try search
-        person1.fund(0); // confirms that the balances have been updated
+    public static void main(String[] args) {
+
+        Clients2.fund("Ann", 100);
+        Clients2.fund("Lane",490);
+        Clients2.fund("Noname", 10);
+        Clients2.fund("Ann", 0); // confirms that the balances have been updated
     }
 }
