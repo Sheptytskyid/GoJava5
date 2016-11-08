fd785f49e391025f12495572db5984384ead2cec
Denys Sheptytskyi <neoxesis@mail.ru>
2016-11-08 23:06:12 +0200

    Homework task 2_3 -modified withdraw() method to accept additional parameter clientName -withdrawal parameter of withdraw() method is now double -fixed styling issues

--- src/module2/homework/task2_3/Clients.java
+++ src/module2/homework/task2_3/Clients.java
@@ -1,51 +1,31 @@
 package module2.homework.task2_3;
 
-/**
- * The Clients class contains the bank clients' names and their account balances
- * the class has a withdraw() method which searches a client's account balance by this client's name
- * and withdraws a specified amount + 5% fee from client's account if the balance is sufficient
- *
- * @author Denys Sheptytskyi
- */
 public class Clients {
 
-    /* define static arrays with client's names and their account balances */
     private static int[] clientBalances = {1200, 250, 2000, 500, 3200};
     private static String[] clientNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
-    private String clientName;
 
-    Clients(String n) {
-        clientName = n;
-    }
 
-    /**
-     * withdraws a specified amount + 5% fee from a client's account if the account balance is sufficient
-     * prints the client's name, sum of withdrawal and the account balance after withdrawal in case of success
-     * prints client's name + NO in case of failure (insufficient funds) for withdrawal
-     *
-     * @param withdrawal
-     */
-    void withdraw(int withdrawal) {
-        int i = 0;      //index to navigate arrays
+    public static void withdraw(String clientName, double withdrawal) {
+        int clientIndex = 0;
 
-        /* search name in clientNames and return its index
-          returns "Owner not found" when index reached the end of clientNames
-         */
-        while (!clientName.equals(clientNames[i])) {
-            i++;
-            if (i == clientNames.length) {
+
+        while (!clientName.equals(clientNames[clientIndex])) {
+            clientIndex++;
+            if (clientIndex == clientNames.length) {
                 System.out.println(clientName + ": Name not found\n");
                 return;
             }
         }
 
-        /* if there are sufficient funds, make a withdrawal, otherwise print name + NO */
-        if (withdrawal * 1.05 <= clientBalances[i]) {
+
+        if (withdrawal * 1.05 <= clientBalances[clientIndex]) {
+
             System.out.println(clientName + " " + withdrawal + " "
-                               + (clientBalances[i] -= withdrawal * 1.05)); //updates the balances array
+                               + (clientBalances[clientIndex] -= withdrawal * 1.05));
             System.out.println();
         } else {
-            System.out.println(clientNames[i] + " NO\n");
+            System.out.println(clientNames[clientIndex] + " NO\n");
         }
     }
 }
--- src/module2/homework/task2_3/ClientsDemo.java
+++ src/module2/homework/task2_3/ClientsDemo.java
@@ -1,24 +1,13 @@
 package module2.homework.task2_3;
 
-/**
- * The ClientsDemo class is used to test the functionality of the Clients class
- *
- * @author Denys Sheptytskyi
- */
+
 public class ClientsDemo {
+
     public static void main(String[] args) {
-        /*
-         * creates 4 clients as objects of Clients class and tries to make a withdrawal
-         */
 
-        Clients person1 = new Clients("Ann");
-        Clients person2 = new Clients("Oww");
-        Clients person3 = new Clients("Denys");     //non-existent client name
-        Clients person4 = new Clients("Lane");
-
-        person1.withdraw(100);
-        person2.withdraw(490);
-        person3.withdraw(10);       //try searching for a non-existent name
-        person4.withdraw(200);
+        Clients.withdraw("Ann", 100);
+        Clients.withdraw("Lane", 490);
+        Clients.withdraw("Noname", 10);
+        Clients.withdraw("Ann", 140);
     }
 }
