f38e0cfd8b5b332ceb0f53d0e591c85be2defecb
Denys Sheptytskyi <neoxesis@mail.ru>
2016-11-08 23:13:45 +0200

    Homework task 3_2 -optimized check() method of Adder class

--- src/module3/homework/task3_2/Adder.java
+++ src/module3/homework/task3_2/Adder.java
@@ -3,10 +3,6 @@ package module3.homework.task3_2;
 public class Adder extends Arithmetic {
 
     static boolean check (Integer a, Integer b) {
-        if (a >= b) {
-            return true;
-        } else {
-            return false;
-        }
+        return a >= b;
     }
 }
