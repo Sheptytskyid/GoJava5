88b24c6eadb216181f0ed200414b427029713738
Denys Sheptytskyi <neoxesis@mail.ru>
2016-11-08 23:09:43 +0200

    Homework task 3_2 -first commit

---
+++ src/module3/homework/task3_2/Adder.java
@@ -0,0 +1,12 @@
+package module3.homework.task3_2;
+
+public class Adder extends Arithmetic {
+
+    static boolean check (Integer a, Integer b) {
+        if (a >= b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
