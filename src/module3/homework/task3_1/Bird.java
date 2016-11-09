705dc6ea3b021b33571cf2e901a354ab2febd63f
Denys Sheptytskyi <neoxesis@mail.ru>
2016-11-09 20:36:58 +0200

    Homework task 3_1 -modified actions array, included class name as the last element

--- src/module3/homework/task3_1/Bird.java
+++ src/module3/homework/task3_1/Bird.java
@@ -2,7 +2,8 @@ package module3.homework.task3_1;
 
 public class Bird {
     public static void main(String[] args) {
-        String[] actions = {"walking", "flying", "singing", "Bird"};
+        Bird obj = new Bird();
+        String[] actions = {"walking", "flying", "singing", obj.getClass().getSimpleName()};
         for (int i = 0; i < actions.length; i++) {
             sing(actions[i]);
         }
