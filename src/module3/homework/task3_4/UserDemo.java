54097954a790b0a5ff14dc91abb0da71b163a368
Denys Sheptytskyi <neoxesis@mail.ru>
2016-11-09 23:26:34 +0200

    Homework task 3_4 -initial commit

---
+++ src/module3/homework/task3_4/UserDemo.java
@@ -0,0 +1,29 @@
+package module3.homework.task3_4;
+
+import javax.sql.rowset.serial.SerialJavaObject;
+
+public class UserDemo {
+
+    public static void main(String[] args) {
+        User Sheptytskyid = new User("Denys Sheptytskyi", "OTP Capital", "UAH", 1_000, 8 * 12 + 10, 100);
+
+
+        Sheptytskyid.setCompanyName("LLC \"AMC \"OTP Capital\"");
+
+
+        System.out.println("User: " + Sheptytskyid.getName());
+        System.out.println("Employer: " + Sheptytskyid.getCompanyName() + " (" + Sheptytskyid.companyNameLength()
+                            + " chars)");
+        System.out.println("Term of employment: " + Sheptytskyid.getMonthsOfEmployment() + " months");
+        System.out.println("Account balance before salary: " + Sheptytskyid.getBalance());
+        System.out.println("Salary: " + Sheptytskyid.getSalary());
+        Sheptytskyid.paySalary();
+        Sheptytskyid.monthIncreaser(1);
+        System.out.println("Account balance after salary: " + Sheptytskyid.getBalance());
+        System.out.println("New term of employment: " + Sheptytskyid.getMonthsOfEmployment() + " months");
+        int withdrawal = 1000;
+        Sheptytskyid.withdraw(withdrawal);
+        System.out.println("Account balance after withdrawing " + Sheptytskyid.getCurrency() + " "
+                            + withdrawal + ": " + Sheptytskyid.getBalance());
+    }
+}
