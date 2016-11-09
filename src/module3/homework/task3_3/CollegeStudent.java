86f24b3519adb3b916b28931ec74e5ec516aa77d
Denys Sheptytskyi <neoxesis@mail.ru>
2016-11-09 22:17:59 +0200

    Homework task 3_3 -first commit

---
+++ src/module3/homework/task3_3/CollegeStudent.java
@@ -0,0 +1,49 @@
+package module3.homework.task3_3;
+
+public class CollegeStudent extends Student {
+
+    private String collegeName;
+    private int rating;
+    private long id;
+
+
+    public CollegeStudent(String firstName, String lastName, int group) {
+        super(firstName, lastName, group);
+    }
+
+    public CollegeStudent(String lastName, Course[] coursesTaken) {
+        super(lastName, coursesTaken);
+    }
+
+    public CollegeStudent(String collegeName, int rating, long id) {
+        super(null, null, 0);
+        this.collegeName = collegeName;
+        this.rating = rating;
+        this.id = id;
+    }
+
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/module3/homework/task3_3/Course.java
@@ -0,0 +1,58 @@
+package module3.homework.task3_3;
+
+import java.util.Date;
+
+public class Course {
+
+    private Date startDate;
+    private String name;
+    private String teacherName;
+    private int hoursDuration;
+
+
+    public Course(Date startDate, String name) {
+        this.startDate = startDate;
+        this.name = name;
+        hoursDuration = 0;
+    }
+
+    public Course(int hoursDuration, String name, String teacherName) {
+        this.hoursDuration = hoursDuration;
+        this.name = name;
+        this.teacherName = teacherName;
+    }
+
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public int getHoursDuration() {
+        return hoursDuration;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public void setHoursDuration(int hoursDuration) {
+        this.hoursDuration = hoursDuration;
+    }
+}
+
