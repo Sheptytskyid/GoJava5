9131c60cf15e33bca6e9a6de99516a33c16f5dc1
Denys Sheptytskyi <neoxesis@mail.ru>
2016-11-09 20:54:09 +0200

    Homework task 3_1 -fixed styling issues

--- src/module3/homework/task3_1/Bird.java
+++ src/module3/homework/task3_1/Bird.java
@@ -1,9 +1,11 @@
 package module3.homework.task3_1;
 
 public class Bird {
+
     public static void main(String[] args) {
         Bird obj = new Bird();
         String[] actions = {"walking", "flying", "singing", obj.getClass().getSimpleName()};
+
         for (int i = 0; i < actions.length; i++) {
             sing(actions[i]);
         }
