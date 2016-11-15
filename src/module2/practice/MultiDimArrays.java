503d2ea30bb61e795877754f30b10a28ceb4a59b
Denys Sheptytskyi <neoxesis@mail.ru>
2016-11-16 00:21:13 +0200

    Module 3 practice -initial commit

--- src/module2/practice/MultiDimArrays.java
+++ src/module2/practice/MultiDimArrays.java
@@ -11,9 +11,14 @@ public class MultiDimArrays {
             array[i] = createRandomArray(5);
         }
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(Arrays.toString(array[i]));
+        printArray(array);
+
+        for (int i = 0; i < (int)array.length/2+1; i++) {
+            transposeArray(array[i], array[array.length - 1 - i]);
         }
+        System.out.println();
+        printArray(array);
+
     }
 
     private static int[] createRandomArray(int length) {
@@ -22,5 +27,26 @@ public class MultiDimArrays {
             array[i] = (int) (Math.random() * 100);
         }
         return array;
+    }
+
+    private static void transposeArray(int[] array1, int[] array2) {
+        if (array1 == array2) {
+            for (int i = 0; i < (int) array1.length/2; i++) {
+                int t = array1[i];
+                array1[i] = array2[array2.length - 1 - i];
+                array2[array2.length - 1 - i] = t;
+            }
+        }
+        for (int i = 0; i < array1.length; i++) {
+            int t = array1[i];
+            array1[i] = array2[array2.length - 1 - i];
+            array2[array2.length - 1 - i] = t;
+        }
+    }
+
+    private static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(Arrays.toString(array[i]));
+        }
     }
 }
