8137d09230bd76b9ae504fa2e3bbe0bd1dd49602
Denys Sheptytskyi <neoxesis@mail.ru>
2017-01-19 23:16:13 +0200

    module 11 homework -returned exception handling back to Main class to terminate program in case of exception

--- src/main/java/gojava/module11/homework/TextFileOperations.java
+++ src/main/java/gojava/module11/homework/TextFileOperations.java
@@ -15,19 +15,19 @@ public class TextFileOperations {
     private static TextFileOperations fileWithSampleText;
     private File textFile;
 
-    private TextFileOperations() {
+    private TextFileOperations() throws IOException {
         makeEmptyTextFile("Source.txt");
         fillFileWithSampleText();
     }
 
-    public static synchronized TextFileOperations getInstance() {
+    public static synchronized TextFileOperations getInstance() throws IOException {
         if (fileWithSampleText == null) {
             fileWithSampleText = new TextFileOperations();
         }
         return fileWithSampleText;
     }
 
-    private void makeEmptyTextFile(String fileName) {
+    private void makeEmptyTextFile(String fileName) throws IOException {
         textFile = new File(fileName);
         try {
             if (!textFile.createNewFile()) {
@@ -35,40 +35,43 @@ public class TextFileOperations {
             }
         } catch (IOException e) {
             System.out.println("Error creating file!");
+            throw e;
         }
     }
 
-    private void fillFileWithSampleText() {
+    private void fillFileWithSampleText() throws IOException {
         try (PrintWriter fileWriter = new PrintWriter(new FileWriter(textFile))) {
             fileWriter.println("This is the initial text line 1");
             fileWriter.println("This is the initial text line 2");
         } catch (IOException e) {
             System.out.println("Error initialising file!");
+            throw e;
         }
     }
 
-    public File fileContentsReplacer(Map<String, String> wordsToReplace) {
+    public File fileContentsReplacer(Map<String, String> wordsToReplace) throws IOException {
         String outputString = replacer(wordsToReplace);
         return writeContentsToFile(outputString, false);
 
     }
 
-    public File fileContentsMerge(Map<String, String> wordsToReplace) {
+    public File fileContentsMerge(Map<String, String> wordsToReplace) throws IOException {
         String outputString = replacer(wordsToReplace);
         return writeContentsToFile(outputString, true);
     }
 
-    public String readFileContentsToString() {
+    public String readFileContentsToString() throws IOException {
         StringBuilder outputString = new StringBuilder();
         try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(textFile))) {
             outputString.append(bufferedFileReader.lines().collect(Collectors.joining(System.lineSeparator())));
         } catch (IOException e) {
             System.out.println("Error reading file!");
+            throw e;
         }
         return outputString.toString();
     }
 
-    public String replacer(Map<String, String> wordsToReplace) {
+    public String replacer(Map<String, String> wordsToReplace) throws IOException {
         String outputString = readFileContentsToString();
         for (Map.Entry<String, String> entry : wordsToReplace.entrySet()) {
             outputString = outputString.replaceAll(entry.getKey(), entry.getValue());
@@ -76,17 +79,18 @@ public class TextFileOperations {
         return outputString;
     }
 
-    public File writeContentsToFile(String contents, boolean append) {
+    public File writeContentsToFile(String contents, boolean append) throws IOException {
         try (BufferedWriter bufferedFileWriter = new BufferedWriter(new FileWriter(textFile, append))) {
             bufferedFileWriter.write(contents);
             bufferedFileWriter.newLine();
         } catch (IOException e) {
             System.out.println("Error writing to file");
+            throw e;
         }
         return textFile;
     }
 
-    public int checkWord(String word) {
+    public int checkWord(String word) throws IOException {
         return readFileContentsToString().split(word).length - 1;
     }
 
@@ -103,6 +107,7 @@ public class TextFileOperations {
             }
         } catch (IOException e) {
             System.out.println("Error reading file!");
+            throw e;
         } finally {
             if (bufferedFileReader != null) {
                 bufferedFileReader.close();
--- src/main/java/gojava/module11/homework/TextFileOperationsDemo.java
+++ src/main/java/gojava/module11/homework/TextFileOperationsDemo.java
@@ -1,5 +1,6 @@
 package gojava.module11.homework;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,18 +10,22 @@ public class TextFileOperationsDemo {
         Map<String, String> wordsToReplace = new HashMap<>();
         wordsToReplace.put("initial", "resulting");
         wordsToReplace.put("line", "string");
-        TextFileOperations fileWithSampleText = TextFileOperations.getInstance();
-        System.out.println("Initial file contents:\n" + fileWithSampleText.readFileContentsToString());
-        System.out.println("\nString with replaced words:");
-        System.out.println(fileWithSampleText.replacer(wordsToReplace));
-        System.out.print("\nRewriting file with the new string... ");
-        fileWithSampleText.fileContentsReplacer(wordsToReplace);
-        System.out.println("Done!");
-        System.out.print("Appending new string to file... ");
-        fileWithSampleText.fileContentsMerge(wordsToReplace);
-        System.out.println("Done!");
-        System.out.println("\nResulting file contents:\n" + fileWithSampleText.readFileContentsToString());
-        System.out.println("\nNumber of occurrences of word \"This\": " + fileWithSampleText.checkWord("This"));
-        System.out.println("Number of occurrences of word \"Denys\": " + fileWithSampleText.checkWord("Denys"));
+        try {
+            TextFileOperations fileWithSampleText = TextFileOperations.getInstance();
+            System.out.println("Initial file contents:\n" + fileWithSampleText.readFileContentsToString());
+            System.out.println("\nString with replaced words:");
+            System.out.println(fileWithSampleText.replacer(wordsToReplace));
+            System.out.print("\nRewriting file with the new string... ");
+            fileWithSampleText.fileContentsReplacer(wordsToReplace);
+            System.out.println("Done!");
+            System.out.print("Appending new string to file... ");
+            fileWithSampleText.fileContentsMerge(wordsToReplace);
+            System.out.println("Done!");
+            System.out.println("\nResulting file contents:\n" + fileWithSampleText.readFileContentsToString());
+            System.out.println("\nNumber of occurrences of word \"This\": " + fileWithSampleText.checkWord("This"));
+            System.out.println("Number of occurrences of word \"Denys\": " + fileWithSampleText.checkWord("Denys"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
