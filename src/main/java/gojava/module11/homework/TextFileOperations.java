7ec83bd661b5910de1ea5b13345b72405a5684c4
Denys Sheptytskyi <neoxesis@mail.ru>
2017-01-19 17:35:08 +0200

    module 11 homework -Exceptions are now handled within methods of TextFileOperations -optimized readFileContentsToString by using Stream and StringBuilder

--- src/main/java/gojava/module11/homework/TextFileOperations.java
+++ src/main/java/gojava/module11/homework/TextFileOperations.java
@@ -8,45 +8,67 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TextFileOperations {
 
     private static TextFileOperations fileWithSampleText;
     private File textFile;
 
-    private TextFileOperations() throws IOException {
+    private TextFileOperations() {
         makeEmptyTextFile("Source.txt");
         fillFileWithSampleText();
     }
 
-    public static synchronized TextFileOperations getInstance() throws IOException {
+    public static synchronized TextFileOperations getInstance() {
         if (fileWithSampleText == null) {
             fileWithSampleText = new TextFileOperations();
         }
         return fileWithSampleText;
     }
 
-    private void makeEmptyTextFile(String fileName) throws IOException {
+    private void makeEmptyTextFile(String fileName) {
         textFile = new File(fileName);
         try {
             if (!textFile.createNewFile()) {
                 new PrintWriter(fileName).close();
             }
         } catch (IOException e) {
-            throw new IOException("Error while creating file!", e);
+            System.out.println("Error creating file!");
         }
     }
 
-    private void fillFileWithSampleText() throws IOException {
+    private void fillFileWithSampleText() {
         try (PrintWriter fileWriter = new PrintWriter(new FileWriter(textFile))) {
             fileWriter.println("This is the initial text line 1");
             fileWriter.println("This is the initial text line 2");
         } catch (IOException e) {
-            throw new IOException("Error while initialising file!", e);
+            System.out.println("Error initialising file!");
         }
     }
 
-    public String replacer(Map<String, String> wordsToReplace) throws IOException {
+    public File fileContentsReplacer(Map<String, String> wordsToReplace) {
+        String outputString = replacer(wordsToReplace);
+        return writeContentsToFile(outputString, false);
+
+    }
+
+    public File fileContentsMerge(Map<String, String> wordsToReplace) {
+        String outputString = replacer(wordsToReplace);
+        return writeContentsToFile(outputString, true);
+    }
+
+    public String readFileContentsToString() {
+        StringBuilder outputString = new StringBuilder();
+        try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(textFile))) {
+            outputString.append(bufferedFileReader.lines().collect(Collectors.joining(System.lineSeparator())));
+        } catch (IOException e) {
+            System.out.println("Error reading file!");
+        }
+        return outputString.toString();
+    }
+
+    public String replacer(Map<String, String> wordsToReplace) {
         String outputString = readFileContentsToString();
         for (Map.Entry<String, String> entry : wordsToReplace.entrySet()) {
             outputString = outputString.replaceAll(entry.getKey(), entry.getValue());
@@ -54,60 +76,38 @@ public class TextFileOperations {
         return outputString;
     }
 
-    public File fileContentsReplacer(Map<String, String> wordsToReplace) throws IOException {
-        String outputString = replacer(wordsToReplace);
-        return writeContentsToFile(outputString, false);
-
-    }
-
-    public File fileContentsMerge(Map<String, String> wordsToReplace) throws IOException {
-        String outputString = replacer(wordsToReplace);
-        return writeContentsToFile(outputString, true);
-    }
-
-    public int checkWord(String word) throws IOException {
-        return readFileContentsToString().split(word).length - 1;
-    }
-
-    public File writeContentsToFile(String contents, boolean append) throws IOException {
-        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(textFile, append))) {
-            fileWriter.write(contents);
-            fileWriter.newLine();
+    public File writeContentsToFile(String contents, boolean append) {
+        try (BufferedWriter bufferedFileWriter = new BufferedWriter(new FileWriter(textFile, append))) {
+            bufferedFileWriter.write(contents);
+            bufferedFileWriter.newLine();
+        } catch (IOException e) {
+            System.out.println("Error writing to file");
         }
         return textFile;
     }
 
-    public String readFileContentsToString() throws IOException {
-        String outputString = "";
-        String textLine;
-        try (BufferedReader fileReader = new BufferedReader(new FileReader(textFile))) {
-            while ((textLine = fileReader.readLine()) != null) {
-                outputString += textLine + System.lineSeparator();
-            }
-            outputString = outputString.trim();
-        } catch (IOException e) {
-            throw new IOException("Error while reading file!", e);
-        }
-        return outputString;
+    public int checkWord(String word) {
+        return readFileContentsToString().split(word).length - 1;
     }
 
 
     public String readFileContentsToStringJava6() throws IOException {
-        String outputString = "";
+        StringBuilder outputString = new StringBuilder();
         String textLine;
-        BufferedReader fileReader = null;
+        BufferedReader bufferedFileReader = null;
         try {
-            fileReader = new BufferedReader(new FileReader(textFile));
-            while ((textLine = fileReader.readLine()) != null) {
-                outputString += textLine + System.lineSeparator();
+            bufferedFileReader = new BufferedReader(new FileReader(textFile));
+            while ((textLine = bufferedFileReader.readLine()) != null) {
+                outputString.append(textLine);
+                outputString.append(System.lineSeparator());
             }
         } catch (IOException e) {
-            throw new IOException("Error while reading file!", e);
+            System.out.println("Error reading file!");
         } finally {
-            if (fileReader != null) {
-                fileReader.close();
+            if (bufferedFileReader != null) {
+                bufferedFileReader.close();
             }
         }
-        return outputString;
+        return outputString.toString();
     }
 }
